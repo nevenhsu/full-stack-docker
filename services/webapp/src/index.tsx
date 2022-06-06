3dacf23dbd446be358a91e0e200b45653b4717dd
nevenhsu <space.man0318@gmail.com>
2022-06-06 17:09:43 +0800

    fix: use ReactDOM.createRoot

--- services/webapp/src/index.tsx
+++ services/webapp/src/index.tsx
@@ -1,21 +1,22 @@
 import './index.scss'
 import React from 'react'
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
 import { store } from 'store'
 import ThemeProvider from 'theme/ThemeProvider'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+
+root.render(
     <React.StrictMode>
         <Provider store={store}>
             <ThemeProvider>
                 <App />
             </ThemeProvider>
         </Provider>
-    </React.StrictMode>,
-    document.getElementById('root')
+    </React.StrictMode>
 )
 
 // If you want to start measuring performance in your app, pass a function
